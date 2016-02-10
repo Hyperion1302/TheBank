bd5ba8386caa189fecec7e10bbb01a949fcfd2f8
Hyperion1302 <Hyperion1302@yahoo.com.sg>
2016-02-10 14:03:17 +0800

    Version 1.0.0
    #2 - Enabled/Disabled log updated

--- TheBank/src/com/Hyperion1302/TheBank/Core.java
+++ TheBank/src/com/Hyperion1302/TheBank/Core.java
@@ -10,16 +10,22 @@ public class Core extends JavaPlugin {
 	Logger logger = Logger.getLogger("Minecraft");
 	PluginDescriptionFile pdf = this.getDescription();
 	
+	API api = API.getInstance();
+	
 	public void onEnable() {
 		
 		//Log - Plugin Enabled
-		logger.info("TheBank v1 enabled");
+		logger.info(api.colourize("&2" + pdf.getName() + " has been enabled"));
+		logger.info(api.colourize("&bVersion: " + pdf.getVersion()));
+		logger.info(api.colourize("&bDeveloper: " + pdf.getAuthors()));
+		logger.info(api.colourize("&bIf you like this plugin, be sure to go to the Spigot page to review this plugin."));
 	}
 	
 	public void onDisable() {
 		
 		//Log - Plugin Disabled
-		logger.info("TheBank v1 disabled");
+		logger.info(api.colourize("&c" + pdf.getName() + "v" + pdf.getVersion() + " has been disabled"));
+		logger.info(api.colourize("Thanks for using " + pdf.getName()));
 	}
 
 }
